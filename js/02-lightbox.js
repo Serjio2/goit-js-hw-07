9f5dd56013f24860c0578f867e0a07a7964c82e1
Serhii Kondratenko <k.serjio@ukr.net>
2023-06-12 00:19:06 +0300

    Fixed 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,8 +16,8 @@ function createListElementsGalery(items) {
     return items.map( ({preview, original, description}) => {
     return `
     <li class="gallery__item">
-        <a class="gallery__link" href="${preview}">
-        <img class="gallery__image" src="${original}" alt="${description}" />
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
     </li>`;
 }).join("");
